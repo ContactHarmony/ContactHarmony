1b17d754e917f4b4bcc3f8f9704fff49bc39ebcd
tom-kap <tomkaplan.software@gmail.com>
2025-03-15 19:10:15 -0400

    Added basic layout

---
+++ src/app_layout.py
@@ -0,0 +1,27 @@
+import flet as ft
+
+class AppLayout(ft.Row):
+    def __init__(self, app, page: ft.Page, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.app = app
+        self.page = page
+        self._active_view: ft.Control = ft.Column(
+            controls=[ft.Text("Active View")],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        )
+        self.controls = [self.active_view]
+
+    @property
+    def active_view(self):
+        return self._active_view
+
+    @active_view.setter
+    def active_view(self, view):
+        self._active_view = view
+        self.update()
+
+    def toggle_nav_rail(self, e):
+        self.sidebar.visible = not self.sidebar.visible
+        self.toggle_nav_rail_button.selected = not self.toggle_nav_rail_button.selected
+        self.page.update()
--- src/main.py
+++ src/main.py
@@ -1,27 +1,76 @@
 import flet as ft
+from app_layout import AppLayout
 
-def main(page: ft.Page):
-    page.title = "ContactHarmony"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-
-    TEXT_SIZE = 16
-
-    def connect_account_click(e):
-        #TODO
-        page.update()
-
-    page.add(
-        ft.Column(
-            [
-                ft.Text(value="No contacts backed-up. Connect an account to get started!",
-                        size=TEXT_SIZE),
-                ft.ElevatedButton("Connect Account", icon=ft.Icons.ADD, on_click=connect_account_click),
+class ContactHarmonyApp(AppLayout):
+    def __init__(self, page: ft.Page):
+        self.page = page
+        self.appbar = ft.AppBar(
+            leading=ft.Icon(ft.Icons.ACCOUNT_CIRCLE),
+            leading_width=100,
+            title=ft.Text("ContactHarmony",size=32, text_align="start"),
+            center_title=False,
+            toolbar_height=75,
+            bgcolor=ft.Colors.LIGHT_BLUE_ACCENT_700,
+            actions=[
+                ft.Container(
+                    content=ft.FilledTonalButton(
+                        "Connect Account",
+                        on_click=self.connect_account
+                    ),
+                    margin=ft.margin.only(left=50, right=25)
+                )
             ],
-            alignment=ft.MainAxisAlignment.CENTER,
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
         )
-    )
+        self.page.appbar = self.appbar
+        self.page.update()
+        super().__init__(
+            self,
+            self.page,
+            tight=True,
+            expand=True,
+            vertical_alignment=ft.CrossAxisAlignment.START,
+        )
+    
+    def connect_account(self, e):
+        def close_dlg(e):
+            if fieldEmail.value == "" or fieldApplicationPassword.value == "":
+                fieldEmail.error_text = "Please provide Gmail Address"
+                fieldApplicationPassword.error_text = "Please provide password"
+                self.page.update()
+                return
+            else:
+                self.fetch_contacts(fieldEmail.value, fieldApplicationPassword.value)
+                self.page.close(dialog)
+                self.page.update()
 
+        fieldEmail = ft.TextField(label="Gmail Address")
+        fieldApplicationPassword = ft.TextField(label="Application Password", password=True)
 
-ft.app(main)
+        dialog = ft.AlertDialog(
+            title=ft.Text("Please enter your Gmail address and application password"),
+            content=ft.Column(
+                [
+                    fieldEmail,
+                    fieldApplicationPassword,
+                    ft.ElevatedButton(text="Connect", on_click=close_dlg),
+                ],
+                tight=True,
+            ),
+            on_dismiss=lambda e: print("Modal dialog dismissed!"),
+        )
+        self.page.open(dialog)
+
+    def fetch_contacts(self, gmail, applicationPassword):
+        #TODO
+        return True
+
+if __name__ == "__main__":
+ 
+    def main(page: ft.Page):
+ 
+        page.title = "ContactHarmony"
+        app = ContactHarmonyApp(page)
+        page.add(app)
+        page.update()
+ 
+    ft.app(main)
