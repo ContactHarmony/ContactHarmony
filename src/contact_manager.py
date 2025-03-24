f16794014225efc56d77487cb4bc61b27be0867f
tom-kap <tomkaplan.software@gmail.com>
2025-03-24 12:10:49 -0400

    updated requirements.txt

--- src/contact_manager.py
+++ src/contact_manager.py
@@ -1,7 +1,7 @@
 import os
 from helpers import Account
 import getGoogleContacts as google
-
+import vobject
 
 class ContactManager():
     def __init__(self):
@@ -9,7 +9,7 @@ class ContactManager():
         self.connectedAccounts = {}
     
     def connect_account(self, account: Account):
-        '''Attempt to connect an account to the ContactManager.'''
+        '''attempt to connect an account to the ContactManager.'''
 
         status = False
         if account in self.connectedAccounts:
@@ -29,7 +29,19 @@ class ContactManager():
         else:
             os.remove(backupPath)   #TODO if used on existing account, return to previous version
             raise Exception(f"Failed to fetch contacts from {account.address}")
-        
+
+    def get_connected_accounts(self):
+        '''returns a list of connected accounts'''
+        return list(self.connectedAccounts.keys())
+    
+    def get_account_contacts(self, account: Account):
+        '''returns a list of account contacts'''
+        #TODO make a contact class instead of returning the raw vcf data
+        #TODO figure out vobject
+        vcfFile = open(self.connectedAccounts[account])
+        vcfContact = vobject.readOne(vcfFile)
+        return None
+    
     def refresh(self):
         '''refetch contacts for all connected accounts'''
         for account in self.connectedAccounts:
